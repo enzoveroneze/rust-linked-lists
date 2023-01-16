f91037970e7a9ae499ca9ac60f50eef42e9e6157
Enzo Veroneze <enzoveroneze@gmail.com>
2023-01-16 18:23:28 -0300

    into_iter,  next

--- src/second.rs
+++ src/second.rs
@@ -9,6 +9,8 @@ struct Node<T> {
 
 type Link<T> = Option<Box<Node<T>>>;
 
+pub struct IntoIter<T>(List<T>);
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: Link::None }
@@ -31,6 +33,16 @@ impl<T> List<T> {
     }
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|nd| &mut nd.elem)
+    }
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
     }
 }
 
@@ -74,5 +86,17 @@ mod test {
         assert_eq!(list.peek_mut(), Some(&mut 3));
         list.peek_mut().map(|n| *n = 42);
         assert_eq!(list.peek(), Some(&42));
+    }
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
