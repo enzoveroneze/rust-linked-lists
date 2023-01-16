80764002914a4ca0a14e5f8cbd53bdfe1c6ef682
Enzo Veroneze <enzoveroneze@gmail.com>
2023-01-16 16:57:03 -0300

    peek mutable

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 # rust-linked-lists
+=======
+# rust-linked-lists
+
+Implementation of the Learning Rust With Entirely Too Many Linked Lists tutorial.
+>>>>>>> 7411892 (peek mutable)
---
+++ src/second.rs
@@ -0,0 +1,65 @@
+pub struct List<T> {
+    head: Link<T>,
+}
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: Link::None }
+    }
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(Node {
+            elem,
+            next: self.head.take(),
+        });
+        self.head = Link::Some(new_node);
+    }
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.elem
+        })
+    }
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|nd| &nd.elem)
+    }
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|nd| &mut nd.elem)
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut cur_link = self.head.take();
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = boxed_node.next.take();
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn basics() {
+        use super::List;
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        list.push(4);
+        list.push(5);
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
